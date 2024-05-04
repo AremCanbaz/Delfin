19318c00f374b23a6235bc3963d73258bd5901ae
AC25-C <151057176+AC25-C@users.noreply.github.com>
2024-05-04 14:48:47 +0200

    Ny subklasse til member. test af getprice() mangler lidt endnu.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 public class Main{
-    static Konkurrencesvømmere motionist = new Konkurrencesvømmere(1,"a","a",1,1,1,true,true,true,false,1,1,12,1990);
+    static Kontigent motionist = new Kontigent(1,"a","a",1,1,1,true,true,true,false, 0);
+    static Kontigent motionist1 = new Kontigent(1,"a","a",1,1,1,true,true,false,true, 0);
+    static Kontigent motionist3 = new Kontigent(1,"a","a",1,1,1,false,true,true,false, 0);
+    static Kontigent motionist4 = new Kontigent(1,"a","a",1,1,1,false,false,false,false, 0);
     public static void main(String[] args) {
         String a;
         String c;
         IDPassword idpassword = new IDPassword();
         LoginPage Loginpage = new LoginPage(idpassword.getLogininfo());
-        String getFulldate = motionist.getFulldate();
-        System.out.println(getFulldate);
+        int getPrice = motionist.getPrice();
+        int getPrice1 = motionist1.getPrice();
+        int getPrice3 = motionist3.getPrice();
+        int getPrice4 = motionist4.getPrice();
 
+        System.out.println(motionist.getPrice());
+        System.out.println(motionist1.getPrice());
+        System.out.println(motionist3.getPrice());
+        System.out.println(motionist4.getPrice());
     }
 }
