1a3b0dc31f7ebfd2d24b79e47d4cd3d5ff798dcf
AC25-C <151057176+AC25-C@users.noreply.github.com>
2024-05-04 21:25:23 +0200

    Lavet hele framet, dog skal der finpudsning til samt at komponenterne for fremvisning af data skal indsættes.

--- src/WelcomePage.java
+++ src/WelcomePage.java
@@ -7,13 +7,19 @@ public class WelcomePage implements ActionListener {
 
     JFrame homeFrame = new JFrame();
     JLabel welcomeLabel = new JLabel();
-    JButton logoutbButton = new JButton();
+    JButton logoutButton = new JButton();
     JPanel topPanel = new JPanel();
     JButton createMemberButton = new JButton();
     JButton memberOverviewButton = new JButton();
     JButton paymentOverviewButton = new JButton();
-    JButton  recordsOverviewButton= new JButton();
+    JButton recordsOverviewButton= new JButton();
     JButton createRecordButton = new JButton();
+    JPanel createRecordFrame = new JPanel();
+    JPanel recordFrame = new JPanel();
+    JPanel memberFrame = new JPanel();
+    JPanel paymentFrame = new JPanel();
+    JPanel createMemberFrame = new JPanel();
+
 
 
 
@@ -24,13 +30,13 @@ public class WelcomePage implements ActionListener {
         topPanel.setBackground(Color.blue);
 
 
-        welcomeLabel.setBounds(100,100,200,35);
+        welcomeLabel.setBounds(0,0,200,35);
         welcomeLabel.setFont(new Font(null,Font.PLAIN,25));
         welcomeLabel.setText("Hi " + "\n" +  userID);
 
-        logoutbButton.setBounds(1420,0,100,25);
-        logoutbButton.addActionListener(this);
-        logoutbButton.setText("Log out");
+        logoutButton.setBounds(1420,0,100,25);
+        logoutButton.addActionListener(this);
+        logoutButton.setText("Log out");
 
         createMemberButton.setBounds(100,100,100,35);
         createMemberButton.addActionListener(this);
@@ -52,6 +58,27 @@ public class WelcomePage implements ActionListener {
         createRecordButton.addActionListener(this);
         createRecordButton.setText("Create Record");
 
+        createRecordFrame.setBounds(0,100,1550,1000);
+        createRecordFrame.setVisible(false);
+        createRecordFrame.setBackground(Color.PINK);
+
+        createMemberFrame.setBounds(0,100,1550,1000);
+        createMemberFrame.setVisible(false);
+        createMemberFrame.setBackground(Color.BLACK);
+
+        recordFrame.setBounds(0,100,1550,1000);
+        recordFrame.setVisible(false);
+        recordFrame.setBackground(Color.RED);
+
+        memberFrame.setBounds(0,100,1550,1000);
+        memberFrame.setVisible(false);
+        memberFrame.setBackground(Color.BLUE);
+
+        paymentFrame.setBounds(0,100,1550,1000);
+        paymentFrame.setVisible(false);
+        paymentFrame.setBackground(Color.yellow);
+
+
 
 
         topPanel.add(welcomeLabel);
@@ -60,9 +87,14 @@ public class WelcomePage implements ActionListener {
         topPanel.add(paymentOverviewButton);
         topPanel.add(createRecordButton);
         topPanel.add(recordsOverviewButton);
-        topPanel.add(logoutbButton,BorderLayout.EAST);
+        topPanel.add(logoutButton,BorderLayout.EAST);
 
 
+        homeFrame.add(memberFrame);
+        homeFrame.add(paymentFrame);
+        homeFrame.add(recordFrame);
+        homeFrame.add(createRecordFrame);
+        homeFrame.add(createMemberFrame);
         homeFrame.add(topPanel);
         homeFrame.add(welcomeLabel);
         homeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -73,14 +105,47 @@ public class WelcomePage implements ActionListener {
 
     }
     public void actionPerformed(ActionEvent e){
-        if(e.getSource()== logoutbButton){
+        if(e.getSource()== logoutButton){
             homeFrame.dispose();
             @SuppressWarnings("unused")
             LoginPage loginPage = new LoginPage(null);
         }
         if(e.getSource()== createMemberButton){
-            CreateMember createMember = new CreateMember();
+            createMemberFrame.setVisible(true);
+            createRecordFrame.setVisible(false);
+            recordFrame.setVisible(false);
+            paymentFrame.setVisible(false);
+            memberFrame.setVisible(false);
         }
+        if(e.getSource()== memberOverviewButton){
+            memberFrame.setVisible(true);
+            createMemberFrame.setVisible(false);
+            createRecordFrame.setVisible(false);
+            recordFrame.setVisible(false);
+            paymentFrame.setVisible(false);
+        }
+        if(e.getSource()== paymentOverviewButton){
+            memberFrame.setVisible(false);
+            createMemberFrame.setVisible(false);
+            createRecordFrame.setVisible(false);
+            recordFrame.setVisible(false);
+            paymentFrame.setVisible(true);
+        }
+        if(e.getSource()== recordsOverviewButton){
+            memberFrame.setVisible(false);
+            createMemberFrame.setVisible(false);
+            createRecordFrame.setVisible(false);
+            recordFrame.setVisible(true);
+            paymentFrame.setVisible(false);
+        }
+        if(e.getSource()== createRecordButton){
+            memberFrame.setVisible(false);
+            createMemberFrame.setVisible(false);
+            createRecordFrame.setVisible(true);
+            recordFrame.setVisible(false);
+            paymentFrame.setVisible(false);
+        }
+
     }
 }
 
